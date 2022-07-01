d692206d2047e3c3ec80ef0f375de7c5768e8842
vporsnev <v.porshnev@gmail.com>
2022-07-01 22:59:25 +0200

    upd

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-// const navbar = document.querySelector('.page-header');
-// window.onscroll = () => {
-//     if (window.scrollY > 50) {
-//         navbar.classList.add('nav-active');
-//     } else {
-//         navbar.classList.remove('nav-active');
-//     }
-// };
-//
-// function changeClass(){
-//       navbar.classList.add('nav-active');
-//     }
-// window.onload = function(){
-//     document.querySelector('.page-header').addEventListener( 'click', changeClass);
-// }
 function reveal() {
   var reveals = document.querySelectorAll(".projects,.work,.skills,.footer");
   for (var i = 0; i < reveals.length; i++) {
