c60178fbd017bc083f7ef9573e89b8fc5735e0cc
vporsnev <v.porshnev@gmail.com>
2022-07-20 16:44:57 +0200

    small upd

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function reveal() {
-  var reveals = document.querySelectorAll(".projects,.work,.skills,.footer");
+  var reveals = document.querySelectorAll(".projects,.work,.skills");
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
